0e634dcc34759a516490a7332b9f6455afc9aca6
Meet Shah <131140868+Meetshahgit@users.noreply.github.com>
2024-01-31 18:03:33 +0400

    new lession

---
+++ 26.modules_and_pip.py
@@ -0,0 +1,36 @@
+# Modules are file that you that contain predefined function or code that User might have created and wants to implement it on a new file
+
+# to import other python file or other file in Python you can use import 
+
+import Functions
+
+# When execuited it will allow you to use all the code defined in that specific file
+# you can call that file and excute all functions or code available in the file
+print(Functions.shopping())
+
+# Python has huge library that you can import to help you get your task done quickly
+# you can check https://www.docs.python.org/ and look for modules
+# there is a whole list of modules that you can use based on your needs and requirement
+
+# there are 2 types of modules
+    # Build in
+    # Internal 
+    # External 
+
+# you can call these internal modules using import function
+# install External Modules you can call PIP
+
+# pip install pandas
+
+# Pip is a program which installed along with your python program 
+# any verison after python 3
+# its is referred as package manager which allows you to install ,update, uninstall
+# PIP is installed from terminal or Command promont
+
+
+# to call the file you can say  import
+import pandas
+# then you can call the functions inside the import how have you wish to use those code 
+
+pandas.read_csv
+
